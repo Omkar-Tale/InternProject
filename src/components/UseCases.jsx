7d6d04a827d08e0c711c00090b1047762137f63b
Omkar Tale <omkarktale1603@gmail.com>
2025-11-11 17:46:43 +0530

    .

--- src/components/UseCases.jsx
+++ src/components/UseCases.jsx
@@ -4,28 +4,29 @@ import UseCaseCard from './UseCaseCard';
 
 const UseCases = () => {
   const { t } = useTranslation();
-  const useCases = t('useCases.cards', { returnObjects: true }); // fetch array
+  const useCases = t('useCasesCards', { returnObjects: true });
 
   return (
     <div className="relative sm:p-20 p-10">
       <div className="text-center flex flex-col">
         <h1 className="font-bold sm:text-[48px] text-[20px] mx-auto font-poppins sm:max-w-full max-w-[60%]">
-          {t('useCases.heading')}
+          {t('useCasesHeading')}
         </h1>
         <p className="text-description font-roboto text-sm sm:text-[18px] text-center mx-auto sm:max-w-full max-w-[90%]">
-          {t('useCases.description')}
+          {t('useCasesDescription')}
         </p>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-16">
-        {useCases.map((feature, index) => (
-          <UseCaseCard
-            key={index}
-            image={feature.image || "default.png"} 
-            title={feature.title}
-            description={feature.description}
-          />
-        ))}
+        {Array.isArray(useCases) &&
+          useCases.map((feature, index) => (
+            <UseCaseCard
+              key={index}
+              image={feature.image || 'default.png'}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
       </div>
     </div>
   );
